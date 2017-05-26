2bacc6ae34833ffc2354925aee486b9ab7d0c2be
inverse <534676033@qq.com>
2017-05-26 11:56:40 +0800

    add pooling setting

--- BidirectionalLSTM.py
+++ BidirectionalLSTM.py
@@ -10,18 +10,23 @@ class BidirectionalLSTM:
             self.forward_lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
             self.backward_lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
 
-    def run(self, data, reuse=False, time_major=False):
+    def run(self, data, reuse=False, time_major=False, pooling=False):
+        time_axis = 0 if time_major else 1
         with tf.variable_scope(self.name):
             with tf.variable_scope("ForwardLSTM", reuse=reuse) as scope:
-                time_axis = 0 if time_major else 1
                 forward_output, state = tf.nn.dynamic_rnn(self.forward_lstm, data, dtype=self.data_type, time_major=time_major, scope=scope)
-                forward_output = tf.reduce_mean(forward_output, time_axis)
+                if pooling == 'mean':
+                    forward_output = tf.reduce_mean(forward_output, time_axis)
+                else:
+                    forward_output = forward_output[-1, :, :] if time_major else forward_output[:, -1, :]
 
             with tf.variable_scope("BackwardLSTM", reuse=reuse) as scope:
-                time_axis = 0 if time_major else 1
                 data = tf.reverse(data, axis=[time_axis])
                 backward_output, state = tf.nn.dynamic_rnn(self.backward_lstm, data, dtype=self.data_type, time_major=time_major, scope=scope)
-                backward_output = tf.reduce_mean(backward_output, time_axis)
+                if pooling == 'mean':
+                    backward_output = tf.reduce_mean(backward_output, time_axis)
+                else:
+                    backward_output = backward_output[-1, :, :] if time_major else backward_output[:, -1, :]
 
             tf.summary.histogram('forward_lstm_output', forward_output)
             tf.summary.histogram('backward_lstm_output', backward_output)
